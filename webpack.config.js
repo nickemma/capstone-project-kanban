be1a6e9ba792b704c732837220c639dccc85ab31
Nicholas Emmanuel Chimaobi <nicholasemmanuel321@gmail.com>
2022-07-28 12:39:25 +0100

    deployed the site and updated readme

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/',
+    publicPath: '/capstone-project-kanban/',
   },
   module: {
     rules: [
