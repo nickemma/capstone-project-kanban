bbbaaddba7b72c119f29e7dfac2923c760760d08
adebowale <ademibowaleadegboye@gmail.com>
2022-07-28 09:54:49 +0100

    fix linters error

--- src/modules/mobile.js
+++ src/modules/mobile.js
@@ -2,9 +2,9 @@ const navbar = document.querySelector('.navbar');
 
 const mobileMenu = () => {
   const menu = document.querySelector('#menu-btn');
-   menu.addEventListener('click', () => {
+  menu.addEventListener('click', () => {
     navbar.classList.toggle('active');
-   });
+  });
 };
 window.onscroll = () => {
   navbar.classList.remove('active');
