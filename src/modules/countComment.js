6bc4745543b2dfdf64eb26c166fcf204ea880a27
adebowale <ademibowaleadegboye@gmail.com>
2022-07-28 10:00:33 +0100

    add  code to return counter

--- src/modules/countComment.js
+++ src/modules/countComment.js
@@ -1,6 +1,6 @@
-// const countComment = (comment) => {
-//   const counter = document.querySelector('.total-comments');
-//   counter.textContent = comment.length;
-//   return counter;
-// };
-// export default countComment;
+const countComment = (comment) => {
+  const counter = document.querySelector('.total-comments');
+  counter.textContent = comment.length;
+  return counter;
+};
+export default countComment;
